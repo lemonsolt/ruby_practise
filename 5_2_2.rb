63f327909bbf4ae54e2eb001978c9bc0c5f59acf
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-10-26 07:38:26 +0000

    [update]

--- 5_2_2.rb
+++ 5_2_2.rb
@@ -1,9 +1,22 @@
+# currencies = {
+#   'japan'=>'yen','us'=>'dollar','india'=>'rupee'
+# }
+
+# currencies.each do |key_value|
+#   key = key_value[0]
+#   value = key_value[1]
+#   puts "#{key}:#{value}"
+# end
+
+
 currencies = {
-  'japan'=>'yen','us'=>'dollar','india'=>'rupee'
+  'japan'=>'yen','us'=>'doller','india'=>'rupee'
 }
+currencies.delete('japan')
 
 currencies.each do |key_value|
   key = key_value[0]
   value = key_value[1]
   puts "#{key}:#{value}"
 end
+
