ca4d70c9971709db6111aed55dfb6f3880f61ed5
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-11-01 13:23:41 +0000

    [update]practise

--- hash.rb
+++ hash.rb
@@ -1,2 +1,18 @@
-tall = {"太郎"=>185,"次郎"=>170,"花子"=>150}
-puts tall["太郎"]
+# tall = {"太郎"=>185,"次郎"=>170,"花子"=>150}
+# puts tall["太郎"]
+
+def buy_burger(menu,drink: true,potato: true)
+  if menu
+    puts "#{menu} burgerを購入"
+  end
+  if drink
+    puts "ドリンクを購入"
+  end
+  if potato
+    puts "ポテトを購入"
+  end
+end
+
+puts buy_burger('cheese',drink: true,potato: true)
+# ▼trueの項目は省略できる（キーワード部分はデフォルト値が無いため省略できない）
+puts buy_burger('fish',potato: false)
